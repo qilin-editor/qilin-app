0f562abf6f8823dc387ea921ac7fe3e8ca084cff
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2016-09-04 21:48:07 +0200

    App dispatcher

---
+++ client/scripts/dispatchers/AppDispatcher.js
@@ -0,0 +1,15 @@
+import { Dispatcher } from "flux";
+
+export const dispatcher = new Dispatcher;
+
+export function register( callback ) {
+    dispatcher.register( callback );
+};
+
+export function waitFor( ids ) {
+    dispatcher.waitFor( ids );
+};
+
+export function dispatch( type, action = {} ) {
+    dispatcher.dispatch( { type, ...action } );
+};
