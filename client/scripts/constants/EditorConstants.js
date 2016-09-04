c2e5495ce23a90ca56cfcadd941b56c0e88c3935
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2016-09-04 21:56:26 +0200

    Editor constants changed

--- client/scripts/constants/EditorConstants.js
+++ client/scripts/constants/EditorConstants.js
@@ -1,16 +1,27 @@
 import keyMirror from "keymirror";
 
 export default keyMirror( {
-    EDITOR_UPDATE           : null,
-    EDITOR_UPDATED          : null,
+    EDITOR_UPDATE_REQUEST           : null,
+    EDITOR_UPDATE_SUCCESS           : null,
+    EDITOR_UPDATE_FAILURE           : null,
 
-    EDITOR_NEXT             : null,
-    EDITOR_PREVIOUS         : null,
+    EDITOR_NEW_FILE_REQUEST         : null,
+    EDITOR_NEW_FILE_SUCCESS         : null,
+    EDITOR_NEW_FILE_FAILURE         : null,
 
-    EDITOR_NEW_FILE         : null,
-    EDITOR_OPEN_FILE        : null,
-    EDITOR_SAVE_FILE        : null,
+    EDITOR_OPEN_FILE_REQUEST        : null,
+    EDITOR_OPEN_FILE_SUCCESS        : null,
+    EDITOR_OPEN_FILE_FAILURE        : null,
 
-    EDITOR_FIND_IN_FILE     : null,
-    EDITOR_REPLACE_IN_FILE  : null,
+    EDITOR_SAVE_FILE_REQUEST        : null,
+    EDITOR_SAVE_FILE_SUCCESS        : null,
+    EDITOR_SAVE_FILE_FAILURE        : null,
+
+    EDITOR_FIND_IN_FILE_REQUEST     : null,
+    EDITOR_FIND_IN_FILE_SUCCESS     : null,
+    EDITOR_FIND_IN_FILE_FAILURE     : null,
+
+    EDITOR_REPLACE_IN_FILE_REQUEST  : null,
+    EDITOR_REPLACE_IN_FILE_SUCCESS  : null,
+    EDITOR_REPLACE_IN_FILE_FAILURE  : null,
 } );
