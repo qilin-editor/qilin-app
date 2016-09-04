daa70b5b55535ac27d88e2d046b9f69416969067
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2016-09-04 21:46:16 +0200

    App and editor constants added

---
+++ client/scripts/constants/EditorConstants.js
@@ -0,0 +1,16 @@
+import keyMirror from "keymirror";
+
+export default keyMirror( {
+    EDITOR_UPDATE           : null,
+    EDITOR_UPDATED          : null,
+
+    EDITOR_NEXT             : null,
+    EDITOR_PREVIOUS         : null,
+
+    EDITOR_NEW_FILE         : null,
+    EDITOR_OPEN_FILE        : null,
+    EDITOR_SAVE_FILE        : null,
+
+    EDITOR_FIND_IN_FILE     : null,
+    EDITOR_REPLACE_IN_FILE  : null,
+} );
