6256217da7223c0ad74411ac0a7fa9032a47a75e
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2016-10-02 01:36:36 +0200

    Linter stuff

--- client/scripts/components/App.react.js
+++ client/scripts/components/App.react.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import className           from "classnames";
+import className            from "classnames";
 
 import AppContent           from "./AppContent.react";
 import AppHeader            from "./AppHeader.react";
