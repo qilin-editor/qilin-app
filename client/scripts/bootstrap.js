3f95daa4b39f16e0f4e0711a5b79df98fc4fccbc
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2016-10-18 22:08:11 +0200

    Code style lint

--- client/scripts/bootstrap.js
+++ client/scripts/bootstrap.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React      from "react";
 import { render } from "react-dom";
-import App from "./components/App.react";
+import App        from "./components/App.react";
+
 const { Window } = nw;
+const MainWin    = Window.get();
+const MainMenu   = require( "./natives/MainMenuNative" )();
 
-const mainWin = Window.get();
-const MainMenu = require("./natives/MainMenuNative")();
+require( "./natives/FileMenuNative" )( MainMenu );
+require( "./natives/FormatMenuNative" )( MainMenu );
+require( "./natives/AboutMenuNative" )( MainMenu );
 
-require("./natives/FileMenuNative")(MainMenu);
-require("./natives/FormatMenuNative")(MainMenu);
-require("./natives/AboutMenuNative")(MainMenu);
+// Menu should be added here, because it appears empty on windows if it is bound
+// to the window before it's children are appended:
+MainWin.menu = MainMenu;
 
-mainWin.menu = MainMenu; //menu should be added here, because it appears empty on windows if it is bound to the window before it's children are appended
-
-render(<App />, document.getElementById("app"));
+render( <App />, document.getElementById( "app" ) );
--- client/scripts/natives/MainMenuNative.js
+++ client/scripts/natives/MainMenuNative.js
@@ -1,13 +1,10 @@
 export default () => {
     const { Menu } = nw;
+    const mainMenu = new Menu( { type : "menubar" } );
 
-    const mainMenu = new Menu({ type: "menubar" });
-
-    if (process.platform === "darwin") { //check if current OS is MacOS, createMacBuiltin crashes the app on Windows and possibly on Linux
-        mainMenu.createMacBuiltin("Qilin");
-    }
-
-
+    // `createMacBuiltin` crashes the app on Windows and possibly on Linux:
+    if ( process.platform === "darwin" )
+        mainMenu.createMacBuiltin( "Qilin" );
 
     return mainMenu;
 };
