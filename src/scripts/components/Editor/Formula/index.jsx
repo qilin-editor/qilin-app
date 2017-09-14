3db138be8a083ed508b8e50c396d7de97cc234a6
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2017-09-14 22:55:20 +0200

    :bug: Fix overlay of top of editor and preview

--- src/scripts/components/Editor/Formula/index.jsx
+++ src/scripts/components/Editor/Formula/index.jsx
@@ -57,83 +57,74 @@ class EditorForumla extends Component {
         });
 
         return (
-            <div
+            <Rnd
+                dragHandleClassName=".qilin-popup-header"
+                minWidth={300}
+                minHeight={300}
+                bounds="parent"
                 style={{
-                    position: "absolute",
-                    top: 50,
-                    bottom: 50,
-                    left: 0,
-                    right: 0,
+                    position: "fixed",
+                    top: "50px",
+                    zIndex: 10,
+                }}
+                default={{
+                    x: 0,
+                    y: 80,
+                    width: 500,
+                    height: 400,
                 }}
             >
-                <Rnd
-                    dragHandleClassName=".qilin-popup-header"
-                    bounds="parent"
-                    minWidth={300}
-                    minHeight={300}
-                    style={{
-                        position: "absolute",
-                        zIndex: 10,
-                    }}
-                    default={{
-                        x: 0,
-                        y: 80,
-                        width: 500,
-                        height: 400,
-                    }}
-                >
-                    <div className={popupClasses}>
-                        <div className="qilin-popup-header">
-                            <div className="qilin-popup-header-title">Math forumla</div>
+                <div className={popupClasses}>
+                    <div className="qilin-popup-header">
+                        <div className="qilin-popup-header-title">Math forumla</div>
 
-                            <div className="qilin-popup-header-close">
-                                <button className="qilin-button is-big" onClick={this.onClose}>Close</button>
-                            </div>
-
-                            <div className="qilin-popup-header-done">
-                                <button className="qilin-button is-big" onClick={this.onDone}>Done</button>
-                            </div>
+                        <div className="qilin-popup-header-close">
+                            <button className="qilin-button is-big" onClick={this.onClose}>Close</button>
                         </div>
 
-                        <Tabs className="qilin-popup-tabs">
-                            <TabList className="qilin-popup-tabs-list">
-                                <Tab className="qilin-popup-tabs-list-item">Editor</Tab>
-                                <Tab className="qilin-popup-tabs-list-item">Output</Tab>
-                            </TabList>
-
-                            <TabPanel className="qilin-popup-content">
-                                <Makrdown
-                                    className="formula-preview"
-                                    content={`\`\`\`katex\n${this.state.value}\n\`\`\``}
-                                />
-
-                                <FormulaEditor
-                                    change={this.onChange}
-                                    symbol={this.state.symbol}
-                                    value={this.state.value}
-                                />
-
-                                <FormulaHelp
-                                    choose={this.onChoose}
-                                />
-                            </TabPanel>
-
-                            <TabPanel className="qilin-popup-content">
-                                <CodeMirror
-                                    className="formula-editor is-full"
-                                    content={`\`\`\`katex\n${this.state.value}\n\`\`\``}
-                                    options={{
-                                        scrollPastEnd: false,
-                                        lineWrapping: false,
-                                        lineNumbers: true,
-                                        readOnly: true,
-                                    }}
-                                />
-                            </TabPanel>
-                        </Tabs>
+                        <div className="qilin-popup-header-done">
+                            <button className="qilin-button is-big" onClick={this.onDone}>Done</button>
+                        </div>
                     </div>
-                </Rnd>
-            </div>
+
+                    <Tabs className="qilin-popup-tabs">
+                        <TabList className="qilin-popup-tabs-list">
+                            <Tab className="qilin-popup-tabs-list-item">Editor</Tab>
+                            <Tab className="qilin-popup-tabs-list-item">Output</Tab>
+                        </TabList>
+
+                        <TabPanel className="qilin-popup-content">
+                            <Makrdown
+                                className="formula-preview"
+                                content={`\`\`\`katex\n${this.state.value}\n\`\`\``}
+                            />
+
+                            <FormulaEditor
+                                change={this.onChange}
+                                symbol={this.state.symbol}
+                                value={this.state.value}
+                            />
+
+                            <FormulaHelp
+                                choose={this.onChoose}
+                            />
+                        </TabPanel>
+
+                        <TabPanel className="qilin-popup-content">
+                            <CodeMirror
+                                className="formula-editor is-full"
+                                content={`\`\`\`katex\n${this.state.value}\n\`\`\``}
+                                options={{
+                                    scrollPastEnd: false,
+                                    lineWrapping: false,
+                                    lineNumbers: true,
+                                    readOnly: true,
+                                }}
+                            />
+                        </TabPanel>
+                    </Tabs>
+                </div>
+            </Rnd>
         );
     }
 }
