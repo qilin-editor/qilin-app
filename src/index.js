0b671e22ab6ffe7ba6b49e01b6bc1d837d9cd73d
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2017-10-06 00:05:46 +0200

    :heavy_plus_sign: Open DevTools by default in dev mode

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './scripts/bootstrap';
 if (process.env.NODE_ENV === 'development') {
   console.info('Development mode enabled');
 
+  const win = nw.Window.get();
+
+  win.showDevTools();
+  win.requestAttention(true);
+
   require('nw-dev');
 }
