5fdf16aa49df4459f3d0d88c7fe9c6e95b68a894
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-04-07 17:56:25 +0200

    Don't show DevTools on reload in dev mode

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import "./scripts/bootstrap";
 if (process.env.NODE_ENV === "development") {
   console.info("Development mode enabled");
 
-  const win = nw.Window.get();
-
-  win.showDevTools();
-  win.requestAttention(true);
+  // const win = nw.Window.get();
+  // win.showDevTools();
+  // win.requestAttention(true);
 
   require("nw-dev");
 }
