6c1821c699a12cb48fdd4537cb4c1934de619974
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-04-25 19:48:44 +0200

    Add normalize and KaTeX styles when exporting as HTML

--- src/scripts/shortcuts/FileShortcuts.js
+++ src/scripts/shortcuts/FileShortcuts.js
@@ -126,6 +126,9 @@ export function getAsHTML(content: string): string {
       <head>
         <title>${title}</title>
 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css" />
+
         <style>
           .content { max-width: 1200px; margin: 0 auto; }
           ${style}
